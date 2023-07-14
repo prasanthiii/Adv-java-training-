1db0e558b2191c7abe67cc708ef1d7487d4a37cb
prasanthiii <116830250+prasanthiii@users.noreply.github.com>
2023-07-14 14:21:57 +0530

    Add files via upload

---
+++ MyClass (1).java
@@ -0,0 +1,31 @@
+public class MyClass {
+    public static void main(String args[]) {
+      int count=0;
+    if(input1%2==0)
+    {
+       count++;
+    }
+    if(input2%2==0)
+    {
+      count++;
+    }
+    if(input3%2==0)
+    {
+       count++;
+    }
+    if(input4%2==0)
+    {
+        count++;
+    }
+    if(input5%2==0)
+    {
+        count++;
+    }
+    if(input6.equals("even"))
+    return count;
+    else
+      return 5-count;
+    }
+}
+    }
+}
