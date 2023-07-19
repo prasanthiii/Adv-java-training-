85d5ee1a94431a6deb58c5b6f16a72d63caf64d0
prasanthiii <116830250+prasanthiii@users.noreply.github.com>
2023-07-19 08:42:45 +0530

    Add files via upload

---
+++ fibonacci.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import  java.util.*;
+class fibonaci
+{
+ public long nthFibonacci(int input1)
+ return 0;
+if(input1==2)
+ return 1;
+ else
+ return nthFibonacci(input1-1)+nthFibonacci(input1-2);
+ }
+}
